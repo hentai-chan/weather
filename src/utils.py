2a143a8afe8eb935e3ede0379679245554ff2d0d
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-01-20 02:57:18 +0100

    Add various utility methods for terminal formatting.

--- src/utils.py
+++ src/utils.py
@@ -2,14 +2,44 @@
 
 import json
 from importlib.resources import path as resource_path
+from itertools import chain
 
-def read_configuration(resource: str, package: str) -> dict:
-     with resource_path(resource, package) as resource_handler:
+import click
+from colorama import Fore, Style
+
+
+def read_configuration() -> dict:
+     with resource_path('src.data', 'config.json') as resource_handler:
          with open(resource_handler, mode='r', encoding='utf-8') as file_handler:
              return json.load(file_handler)
 
-def write_configuration(resource: str, package: str, params: dict) -> None:
-     config = read_configuration(resource, package)
-     with resource_path(resource, package) as resource_handler:
+def write_configuration(params: dict) -> None:
+     config = read_configuration()
+     with resource_path('src.data', 'config.json') as resource_handler:
         with open(resource_handler, mode='w', encoding='utf-8') as file_handler:
             json.dump({**config, **params}, file_handler)
+
+def reset_configuration() -> None:
+    with resource_path('src.data', 'config.json') as resource_handler:
+        with open(resource_handler, mode='w', encoding='utf-8') as file_handler:
+            json.dump({}, file_handler)
+
+def print_dict(title_left: str, title_right: str, table: dict) -> None:
+    invert = lambda x: -x + (1 + len(max(chain(table.keys(), [title_left]), key=len)) // 8)
+    tabs = lambda string: invert(len(string) // 8) * '\t'
+    click.secho(f"\n{title_left}{tabs(title_left)}{title_right}", fg='green')
+    click.echo(f"{len(title_left) * '-'}{tabs(title_left)}{len(title_right) * '-'}")
+    for key, value in table.items():
+            click.echo(f"{key}{tabs(key)}{value}")
+    click.echo()
+
+def color_string(string: str, color: Fore) -> str:
+    return f"{color}{string}{Style.RESET_ALL}"
+
+def print_on_success(message: str, verbose: bool=True) -> None:
+    if verbose:
+        click.secho(f"{Fore.GREEN}{'[  OK  ]'.ljust(10, ' ')}{Style.RESET_ALL}{message}")
+
+def print_on_error(message: str, verbose: bool=True) -> None:
+    if verbose:
+        click.secho(f"{Fore.RED}{'[ ERROR ]'.ljust(10, ' ')}{Style.RESET_ALL}{message}", err=True)
