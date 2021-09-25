091fa7feee75a1d44ccae05e46227bada33260f8
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-09-25 18:44:34 +0200

    Add internal config file

---
+++ src/weather/config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+LOGFILE = 'error.log'
+CONFIGFILE = 'config.json'
+REPORTFILE = 'data.csv'
+
+
+DIM = '\033[2m'
+NORMAL = ''
+BRIGHT = '\033[1m'
+RESET_ALL = '\033[0m'
+
+CYAN = '\033[36m'
+YELLOW = '\033[33m'
+GREEN = '\033[32m'
+RED = '\033[31m'
+MAGENTA = '\033[35m'
